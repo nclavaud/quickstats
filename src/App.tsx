34b02856583748e1d8a523390ae5b2d25c93aedf
Nicolas Clavaud <nclavaud@gmail.com>
2025-01-10 22:30:03 +0100

    feat: Count dates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import './App.css'
+import { useState } from 'react';
 
 function App() {
+  const [dates, setDates] = useState<string[]>([]);
+
+  const onDatesUpdated = (text: string) => {
+    const dates = text.split(/\r?\n/);
+    setDates(dates);
+  };
+
   return (
     <>
-      <h1>QuickStats</h1>
+      <div className="container mx-auto bg-neutral-100">
+        <div className="min-h-screen bg-neutral-100">
+          <header className="bg-pink-700 text-neutral-100 p-4">
+            <h1 className="text-2xl font-bold">QuickStats</h1>
+          </header>
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <p>Paste a list of dates below:</p>
+              <textarea id="dates" onChange={e => onDatesUpdated(e.target.value)} rows={20}></textarea>
+            </div>
+            <div>
+              <table className="table-fixed">
+                <tbody>
+                  <tr>
+                    <th className="border px-2">Count</th>
+                    <td className="border px-2">{dates.length}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
