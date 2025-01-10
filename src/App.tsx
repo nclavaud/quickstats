2104052e4c777a547f8c527bcf002d8f7aab0ec4
Nicolas Clavaud <nclavaud@gmail.com>
2025-01-10 23:51:20 +0100

    feat: Add margins

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function App() {
     <>
       <div className="container mx-auto bg-neutral-100">
         <div className="min-h-screen bg-neutral-100">
-          <header className="bg-pink-700 text-neutral-100 p-4">
+          <header className="bg-pink-700 text-neutral-100 p-4 mb-4">
             <h1 className="text-2xl font-bold">QuickStats</h1>
           </header>
           <div className="grid grid-cols-2 gap-4">
@@ -52,66 +52,72 @@ function App() {
               <textarea id="dates" onChange={e => onDatesUpdated(e.target.value)} rows={20}></textarea>
             </div>
             <div>
-              <table className="table-fixed">
-                <tbody>
-                  <tr>
-                    <th className="border px-2">Count</th>
-                    <td className="border px-2">{dates.length}</td>
-                  </tr>
-                  <tr>
-                    <th className="border px-2">Min</th>
-                    <td className="border px-2">{dates.length ? new Date(collect(dates).min()).toLocaleDateString() : '-'}</td>
-                  </tr>
-                  <tr>
-                    <th className="border px-2">Max</th>
-                    <td className="border px-2">{dates.length ? new Date(collect(dates).max()).toLocaleDateString() : '-'}</td>
-                  </tr>
-                </tbody>
-              </table>
-              <h3>By year</h3>
-              {dates.length ? (
-                <Chart
-                  options={{
-                    chart: {
-                      animations: {
-                        enabled: false,
+              <div className="mb-4">
+                <table className="table-fixed">
+                  <tbody>
+                    <tr>
+                      <th className="border px-2">Count</th>
+                      <td className="border px-2">{dates.length}</td>
+                    </tr>
+                    <tr>
+                      <th className="border px-2">Min</th>
+                      <td className="border px-2">{dates.length ? new Date(collect(dates).min()).toLocaleDateString() : '-'}</td>
+                    </tr>
+                    <tr>
+                      <th className="border px-2">Max</th>
+                      <td className="border px-2">{dates.length ? new Date(collect(dates).max()).toLocaleDateString() : '-'}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="mb-4">
+                <h3>By year</h3>
+                {dates.length ? (
+                  <Chart
+                    options={{
+                      chart: {
+                        animations: {
+                          enabled: false,
+                        },
+                        id: 'chart-count-by-year',
                       },
-                      id: 'chart-count-by-year',
-                    },
-                    xaxis: {
-                      categories: Object.keys(countByYear),
-                    },
-                  }}
-                  series={[{
-                    name: 'count',
-                      data: Object.values(countByYear),
-                  }]}
-                  type="bar"
-                  width="500"
-                />
-              ) : 'n/a'}
-              <h3>By month</h3>
-              {dates.length ? (
-                <Chart
-                  options={{
-                    chart: {
-                      animations: {
-                        enabled: false,
+                      xaxis: {
+                        categories: Object.keys(countByYear),
                       },
-                      id: 'chart-count-by-month',
-                    },
-                    xaxis: {
-                      categories: Object.keys(countByMonth),
-                    },
-                  }}
-                  series={[{
-                    name: 'count',
-                      data: Object.values(countByMonth),
-                  }]}
-                  type="bar"
-                  width="500"
-                />
-              ) : 'n/a'}
+                    }}
+                    series={[{
+                      name: 'count',
+                        data: Object.values(countByYear),
+                    }]}
+                    type="bar"
+                    width="500"
+                  />
+                ) : 'n/a'}
+              </div>
+              <div className="mb-4">
+                <h3>By month</h3>
+                {dates.length ? (
+                  <Chart
+                    options={{
+                      chart: {
+                        animations: {
+                          enabled: false,
+                        },
+                        id: 'chart-count-by-month',
+                      },
+                      xaxis: {
+                        categories: Object.keys(countByMonth),
+                      },
+                    }}
+                    series={[{
+                      name: 'count',
+                        data: Object.values(countByMonth),
+                    }]}
+                    type="bar"
+                    width="500"
+                  />
+                ) : 'n/a'}
+              </div>
             </div>
           </div>
         </div>
