a20c005819bca4be328301f7ad5547c8f56386b8
Nicolas Clavaud <nclavaud@gmail.com>
2025-01-11 13:45:53 +0100

    feat: Generate a series of months

--- src/range.test.ts
+++ src/range.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { generateRangeYears } from './range';
+import { generateRangeMonths, generateRangeYears } from './range';
 
 test('it generates a series of years between two dates', () => {
   const expected = {
@@ -10,11 +10,40 @@ test('it generates a series of years between two dates', () => {
   expect(generateRangeYears(new Date('2023-01-01'), new Date('2025-05-05'))).toEqual(expected);
 });
 
-test('it always generate series in ascending order', () => {
+test('it always generates a series of years in ascending order', () => {
   const expected = {
     '2023': 0,
     '2024': 0,
     '2025': 0,
   };
   expect(generateRangeYears(new Date('2025-01-01'), new Date('2023-05-05'))).toEqual(expected);
+});
+
+test('it generates a series of months between two dates', () => {
+  const expected = {
+    '2024-05': 0,
+    '2024-06': 0,
+    '2024-07': 0,
+  };
+  expect(generateRangeMonths(new Date('2024-05-08'), new Date('2024-07-31'))).toEqual(expected);
+});
+
+test('it always generates a series of months in ascending order', () => {
+  const expected = {
+    '2024-05': 0,
+    '2024-06': 0,
+    '2024-07': 0,
+  };
+  expect(generateRangeMonths(new Date('2024-07-31'), new Date('2024-05-08'))).toEqual(expected);
+});
+
+test('it generates series of months over several years', () => {
+  const expected = {
+    '2023-10': 0,
+    '2023-11': 0,
+    '2023-12': 0,
+    '2024-01': 0,
+    '2024-02': 0,
+  };
+  expect(generateRangeMonths(new Date('2023-10-01'), new Date('2024-02-05'))).toEqual(expected);
 });
--- src/range.ts
+++ src/range.ts
@@ -1,3 +1,21 @@
+export const generateRangeMonths = (start: Date, end: Date): { [key: string]: number } => {
+  if (start > end) {
+    return generateRangeMonths(end, start);
+  }
+
+  const s = new Date(start.getFullYear(), start.getMonth());
+  const e = new Date(end.getFullYear(), end.getMonth());
+
+  const range: { [key: string]: number } = {};
+
+  for (let i = s; i <= e; i = new Date(i.getFullYear(), i.getMonth() + 1)) {
+    const month = i.getMonth() + 1;
+    range[`${i.getFullYear()}-${month < 10 ? '0' : ''}${month}`] = 0;
+  }
+
+  return range;
+};
+
 export const generateRangeYears = (start: Date, end: Date): { [key: string]: number } => {
   if (start > end) {
     return generateRangeYears(end, start);
